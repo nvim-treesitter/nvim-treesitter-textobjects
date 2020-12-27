91953bea861fa9b16593b2d792986ac01ab04dad
AdnoC <adam.r.cutler@gmail.com>
2020-12-26 19:32:51 -0700

    feat: add queries for java

---
+++ queries/java/textobjects.scm
@@ -0,0 +1,39 @@
+(class_declaration
+  body: (class_body) @class.inner) @class.outer
+
+(method_declaration
+  body: (_) @function.inner) @function.outer
+
+(for_statement
+  body: (_)? @loop.inner) @loop.outer
+
+(enhanced_for_statement
+  body: (_)? @loop.inner) @loop.outer
+
+(while_statement
+  body: (_)? @loop.inner) @loop.outer
+
+(do_statement
+  body: (_)? @loop.inner) @loop.outer
+
+(if_statement
+  condition: (_ (expression) @conditional.inner)  @conditional.outer)
+
+(if_statement
+  consequence: (_)? @conditional.inner
+  alternative: (_)? @conditional.inner
+  ) @conditional.outer
+
+(switch_statement
+  body: (_)? @conditional.inner) @conditional.outer
+
+;; blocks
+(block) @block.outer
+
+
+(method_invocation) @call.outer
+(method_invocation (argument_list) @call.inner)
+
+;; parameters
+(formal_parameters
+  (formal_parameter) @parameter.inner)

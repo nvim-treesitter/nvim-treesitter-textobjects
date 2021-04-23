f69c92c874bdbe4bb7993c041d2fa356330b6b9c
Alvaro Muñoz <alvaro@pwntester.com>
2021-04-23 14:23:58 +0200

    Update CodeQL textobjects

--- queries/ql/textobjects.scm
+++ queries/ql/textobjects.scm
@@ -1,7 +1,12 @@
 ;; class textobject
-(dataclass) @class.outer
+(dataclass (typeExpr) (_) @class.inner) @class.outer
 
 ;; function textobject
-(classMember) @function.outer
-(memberPredicate (body) @function.inner)
+(charpred (className) (_) @function.inner) @function.outer
+(memberPredicate (body) @function.inner) @function.outer
 (classlessPredicate (body) @function.inner) @function.outer
+
+;; block textobject
+(classlessPredicate name: (predicateName) @block.outer)
+(memberPredicate name: (predicateName) @block.outer)
+(charpred (className) @block.outer)

786ca8680ecbed11f3f6a157ea411e7ad2ee4d34
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2021-04-23 14:43:25 -0500

    Update Julia queries
    * Update queries for `@parameter_outer` in:
        - call_expression / argument_list
        - function_definition / parameter_list
    * Add queries for:
        - structs
        - math functions
        - macros

--- queries/julia/textobjects.scm
+++ queries/julia/textobjects.scm
@@ -1,16 +1,24 @@
-(compound_expression) @block.outer    ;; begin blocks
+(compound_expression) @block.outer    ; begin blocks
 (let_statement)       @block.outer
 
-(call_expression) @call.outer
+(call_expression
+  (identifier)
+  (argument_list) @parameter.outer) @call.outer
+
+(struct_definition) @class.outer    ; not classes, but close enough
 
 (comment) @comment.outer
 
 (if_statement) @conditional.outer
 
-(function_definition) @function.outer
-(function_expression) @function.outer    ;; lambdas
+(function_definition
+  name: (identifier)
+  parametere: (parameter_list) @parameter.outer) @function.outer
+
+(assignment_expression
+  (call_expression) (_)) @function.outer    ; math functions
+(function_expression)    @function.outer    ; lambdas
+(macro_definition)       @function.outer
 
 (for_statement)    @loop.outer
 (while_statement)  @loop.outer
-
-(argument_list) @parameter.outer

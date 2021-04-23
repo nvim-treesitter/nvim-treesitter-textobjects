c0dcbbcf5841ccba4c6a367ed3d95a4fccc74d2f
Alvaro Muñoz <alvaro@pwntester.com>
2021-04-23 14:34:56 +0200

    update query

--- queries/ql/textobjects.scm
+++ queries/ql/textobjects.scm
@@ -1,12 +1,13 @@
 ;; class textobject
 (dataclass (typeExpr) (_) @class.inner) @class.outer
 
-;; function textobject
+;; predicate textobject
 (charpred (className) (_) @function.inner) @function.outer
 (memberPredicate (body) @function.inner) @function.outer
 (classlessPredicate (body) @function.inner) @function.outer
 
-;; block textobject
-(classlessPredicate name: (predicateName) @block.outer)
-(memberPredicate name: (predicateName) @block.outer)
-(charpred (className) @block.outer)
+;; identifier textobject
+(dataclass name: (className) @identifier.outer)
+(classlessPredicate name: (predicateName) @identifier.outer)
+(memberPredicate name: (predicateName) @identifier.outer)
+(charpred (className) @identifier.outer)

3f1b31f3810ea9d8a729728f1fe0e7600c70d83b
Alvaro Muñoz <alvaro@pwntester.com>
2021-04-23 17:54:36 +0200

    Improve CodeQL support

--- queries/ql/textobjects.scm
+++ queries/ql/textobjects.scm
@@ -1,7 +1,13 @@
 ;; class textobject
-(dataclass) @class.outer
+(dataclass (typeExpr) (_) @class.inner) @class.outer
 
 ;; function textobject
-(classMember) @function.outer
-(memberPredicate (body) @function.inner)
+(charpred (className) (_) @function.inner) @function.outer
+(memberPredicate (body) @function.inner) @function.outer
 (classlessPredicate (body) @function.inner) @function.outer
+
+;; scope name textobject
+(dataclass name: (className) @scopename.inner)
+(classlessPredicate name: (predicateName) @scopename.inner)
+(memberPredicate name: (predicateName) @scopename.inner)
+(charpred (className) @scopename.inner)

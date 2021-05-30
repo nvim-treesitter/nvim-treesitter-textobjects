47bbb2dd697eac4930934c83e6e4b2f8fdf8e5b3
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-05-30 18:39:11 -0400

    Change lookahead parameter to opts in textobject_at_point

--- lua/nvim-treesitter/textobjects/select.lua
+++ lua/nvim-treesitter/textobjects/select.lua
@@ -10,7 +10,7 @@ local M = {}
 
 function M.select_textobject(query_string, keymap_mode)
   local lookahead = configs.get_module("textobjects.select").lookahead
-  local bufnr, textobject = shared.textobject_at_point(query_string, nil, nil, lookahead)
+  local bufnr, textobject = shared.textobject_at_point(query_string, nil, nil, {lookahead = lookahead})
   if textobject then
     ts_utils.update_selection(bufnr, textobject, M.detect_selection_mode(keymap_mode))
   end

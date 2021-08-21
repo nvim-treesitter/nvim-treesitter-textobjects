60089567104cd056498803ae5e0ae9f93f5b15f5
Colin Svingen <swoogan@gmail.com>
2021-08-21 17:28:26 -0400

    Attempt to build

--- queries/zig/textobjects.scm
+++ queries/zig/textobjects.scm
@@ -9,20 +9,6 @@
 (while_expression
   body: (block) @loop.inner) @loop.outer
 
-;; blocks
-(_ (block) @block.inner) @block.outer
-
 ;; comments
 (doc_comment) @comment.outer
 (line_comment) @comment.outer
-
-;; conditionals
-(if_expression
-  consequence: (_ (block) @conditional.inner)
-  alternative: (block)? @conditional.inner) @conditional.outer
-
-(switch_expression 
-  body: (_) @conditional.inner) @conditional.outer
-
-;; calls
-(call_expression (arguments) @call.inner) @call.outer

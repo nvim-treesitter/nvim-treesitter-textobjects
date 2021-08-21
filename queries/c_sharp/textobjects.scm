8fa446485112cb3f23da74e4e706fcdf368b5735
Colin Svingen <swoogan@gmail.com>
2021-08-21 11:36:41 -0400

    Adds more C# captures (#101)
    Adds the basic captures for loops, conditionals and classes.

--- queries/c_sharp/textobjects.scm
+++ queries/c_sharp/textobjects.scm
@@ -1,4 +1,49 @@
+(class_declaration 
+  body: (declaration_list) @class.inner) @class.outer
+
+(struct_declaration
+  body: (declaration_list) @class.inner) @class.outer
 
 (method_declaration
   body: (block) ? @function.inner) @function.outer
 
+(lambda_expression 
+  body: (_) @function.inner) @function.outer
+
+;; loops
+(for_statement
+  body: (_) @loop.inner) @loop.outer
+
+(for_each_statement
+  body: (_) @loop.inner) @loop.outer
+
+(do_statement
+  (block) @loop.inner) @loop.outer
+
+(while_statement
+  (block) @loop.inner) @loop.outer
+
+;; conditionals
+(if_statement
+  consequence: (_)? @conditional.inner
+  alternative: (_)? @conditional.inner) @conditional.outer
+
+(switch_statement
+  body: (switch_body) @conditional.inner) @conditional.outer
+
+;; calls
+(invocation_expression) @call.outer
+
+(invocation_expression 
+  (argument_list) @call.inner)
+
+;; blocks
+(_ (block) @block.inner) @block.outer
+
+;; arguments
+(argument_list 
+  (_) @call.inner)
+
+;; parameters
+(parameter_list
+  (_) @parameter.inner) @parameter.outer

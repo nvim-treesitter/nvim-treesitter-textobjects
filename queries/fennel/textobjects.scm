85e98992bf8766b9a54819a1726397adc76edde9
Pete Sevander <pete.sevander@gmail.com>
2021-09-25 21:34:33 +0300

    Fennel textobjects

---
+++ queries/fennel/textobjects.scm
@@ -0,0 +1,63 @@
+
+; https://fennel-lang.org/reference
+
+(comment) @comment.outer
+
+(_ . "(" ")" .) @statement.outer
+
+; functions
+((fn  . (parameters) . docstring: (_)? . (_) @_start . (_)* . (_)? @_end .)
+ (#make-range! "function.inner" @_start @_end)) @function.outer
+
+((lambda name: (_) . (parameters) . docstring: (_)? . (_) @_start . (_)* . (_)? @_end .)
+ (#make-range! "function.inner" @_start @_end)) @function.outer
+
+(hashfn ["#" "hashfn"] @function.outer.start (_) @function.inner) @function.outer
+
+; parameters
+(parameters (symbol) @parameter.inner)
+
+; only parameter
+((parameters . (symbol) @parameter.outer .))
+
+; start or middle parameter
+((parameters (symbol) @parameter.outer . (symbol)+)
+ (#offset! @parameter.outer 0 0 0 1))
+
+; last parameter
+((parameters (symbol)+ . (symbol) @parameter.outer .)
+ (#offset! @parameter.outer 0 -1 0 0))
+
+
+; call
+((list . [(multi_symbol) (symbol)] @call.inner) @call.outer
+ (#not-any-of? @call.inner "if" "do" "while" "for" "let" "when"))
+
+
+; conditionals
+((list . ((symbol) @_if (#any-of? @_if "if" "when")) . (_) .
+  (_) @_start .
+  (_)* .
+  (_)? @_end .)
+ (#make-range! "conditional.inner" @_start @_end)) @conditional.outer
+
+
+; loops
+((for . (for_clause) .
+  (_) @_start .
+  (_)* .
+  (_)? @_end .)
+ (#make-range! "loop.inner" @_start @_end)) @loop.outer
+
+((each . (each_clause) .
+  (_) @_start .
+  (_)* .
+  (_)? @_end .)
+ (#make-range! "loop.inner" @_start @_end)) @loop.outer
+
+((list . ((symbol) @_while (#eq? @_while "while")) . (_) .
+  (_) @_start .
+  (_)* .
+  (_)? @_end .)
+ (#make-range! "loop.inner" @_start @_end)) @loop.outer
+

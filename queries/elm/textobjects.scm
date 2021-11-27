66b34c147a435d563b6ee86bdb123a031398740d
Bradford Wells <bwells@altalang.com>
2021-11-26 21:28:11 -0700

    Add parameter queries for lists and tuples

--- queries/elm/textobjects.scm
+++ queries/elm/textobjects.scm
@@ -26,7 +26,7 @@
     body: (_)? @function.inner) @function.outer
 )
 
-; Comments
+;; Comments
 [
   (block_comment)
   (line_comment)
@@ -40,19 +40,45 @@
 (case_of_expr
     branch: (case_of_branch) @conditional.inner) @conditional.outer
 
-; Parameters
+;; Parameters
+; type annotations
 ((type_expression
     (arrow) @_start .
-    [
-      (type_ref)
-    ] @parameter.inner
+    (type_ref) @parameter.inner
   )
   (#make-range! "parameter.outer" @_start @parameter.inner))
 
 ((type_expression
-    . [
-      (type_ref)
-    ] @parameter.inner
+    .
+    (type_ref) @parameter.inner
     . (arrow)? @_end
+  )
+  (#make-range! "parameter.outer" @parameter.inner @_end))
+
+; list items
+((list_expr
+    "," @_start .
+    exprList: (_) @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+((list_expr
+    .
+    exprList: (_) @parameter.inner
+    . ","? @_end
+  )
+  (#make-range! "parameter.outer" @parameter.inner @_end))
+
+; tuple items
+((tuple_expr
+    "," @_start .
+    expr: (_) @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+((tuple_expr
+    .
+    expr: (_) @parameter.inner
+    . ","? @_end
   )
   (#make-range! "parameter.outer" @parameter.inner @_end))

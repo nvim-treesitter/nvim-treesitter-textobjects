80c8344c46c8013de0aeb1d6d99eaf8dc2b2625d
Bradford Wells <bwells@altalang.com>
2021-11-26 20:42:09 -0700

    Adds relevant text objects for Elm
    Adds
      * comment.outer
      * conditional.inner
      * conditional.outer
      * function.inner
      * function.outer
      * parameter.inner
      * parameter.outer

---
+++ queries/elm/textobjects.scm
@@ -0,0 +1,58 @@
+;; Functions
+; top level function with type annotation and doc comment
+(
+  (module_declaration)
+  (block_comment) @function.outer.start
+  .
+  (type_annotation)
+  .
+  (value_declaration
+    body: (_)? @function.inner) @function.outer
+)
+
+; top level function with type annotation
+(
+  (module_declaration)
+  (type_annotation) @function.outer.start
+  .
+  (value_declaration
+    body: (_)? @function.inner) @function.outer
+)
+
+; top level function without type annotation
+(
+  (module_declaration)
+  (value_declaration
+    body: (_)? @function.inner) @function.outer
+)
+
+; Comments
+[
+  (block_comment)
+  (line_comment)
+] @comment.outer
+
+; Conditionals
+(if_else_expr
+    exprList: (_)
+    exprList: (_) @conditional.inner) @conditional.outer
+
+(case_of_expr
+    branch: (case_of_branch) @conditional.inner) @conditional.outer
+
+; Parameters
+((type_expression
+    (arrow) @_start .
+    [
+      (type_ref)
+    ] @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+((type_expression
+    . [
+      (type_ref)
+    ] @parameter.inner
+    . (arrow)? @_end
+  )
+  (#make-range! "parameter.outer" @parameter.inner @_end))

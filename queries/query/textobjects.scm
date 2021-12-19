ad64f0deb7cb1f28c4a504a32a27a7b4f425b14a
Santos Gallegos <stsewd@protonmail.com>
2021-12-19 14:14:24 -0500

    Query: add textobjects

---
+++ queries/query/textobjects.scm
@@ -0,0 +1,11 @@
+(comment) @comment.outer
+
+[
+  (named_node)
+  (grouping)
+  (list)
+] @block.outer
+
+(program [(named_node) (grouping) (list)] @statement.outer)
+
+(parameters (_) @parameter.inner)

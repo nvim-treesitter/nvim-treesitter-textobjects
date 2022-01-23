a6843fc0094bd49adfd662475525545e5c4b7f37
Pedro Castro <aspeddro@gmail.com>
2022-01-23 16:31:35 -0300

    add support for r

---
+++ queries/r/textobjects.scm
@@ -0,0 +1,57 @@
+; block
+
+; call
+
+(call) @call.outer (arguments) @call.inner
+
+; class
+
+; comment
+
+(comment) @comment.outer
+
+; conditional
+
+(if
+  condition: (_)? @conditional.inner) @conditional.outer
+
+; function
+
+[
+  (function_definition)
+  (lambda_function)
+] @function.outer
+
+(function_definition
+  [
+    (call)
+    (binary)
+    (brace_list)
+  ] @function.inner) @function.outer
+
+(lambda_function
+  [
+    (call)
+    (binary)
+    (brace_list)
+  ] @function.inner
+) @function.outer
+
+; loop
+
+[
+  (while)
+  (repeat)
+  (for)
+] @loop.outer
+
+(while body: (_) @loop.inner)
+
+(repeat body: (_) @loop.inner)
+
+(for body: (_) @loop.inner)
+
+; parameter
+
+(formal_parameters
+  (identifier) @parameter.inner) @parameter.outer

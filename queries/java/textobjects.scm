009c4ec8550f83cf71d986f0355abc4551e498ca
qsdrqs <qsdrqs@gmail.com>
2022-02-21 21:59:40 -0800

    feat(java): add argument support

--- queries/java/textobjects.scm
+++ queries/java/textobjects.scm
@@ -47,6 +47,15 @@
   . ","? @_end
  (#make-range! "parameter.outer" @_end @parameter.inner))
 
+((argument_list
+  "," @_start .
+  (_) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((argument_list
+  . (_) @parameter.inner
+  . ","? @_end)
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
 [
   (line_comment)
   (block_comment)

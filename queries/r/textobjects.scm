7e9d2cfb9591375930fb31bfb9393442980a8705
Pedro Castro <aspeddro@gmail.com>
2022-04-11 23:02:01 -0300

    fix(r): parameter and argument

--- queries/r/textobjects.scm
+++ queries/r/textobjects.scm
@@ -54,11 +54,22 @@
 ; parameter
 
 ((formal_parameters
+  "," @_start .
+  (_) @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+((formal_parameters
   . (_) @parameter.inner
   . ","? @_end
   )
   (#make-range! "parameter.outer" @parameter.inner @_end))
 
+((arguments
+  "," @_start .
+  (_) @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
 
 ((arguments
   . (_) @parameter.inner

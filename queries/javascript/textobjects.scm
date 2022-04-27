b1e850b77e57b2720c06d523d6fc4776ad6a5608
Melker Österberg <melkerosterberg@gmail.com>
2022-04-27 17:53:39 +0200

    Exclude curly braces in JavaScript arrow functions

--- queries/javascript/textobjects.scm
+++ queries/javascript/textobjects.scm
@@ -17,7 +17,11 @@
   (function_declaration) @function.outer) @function.outer.start
 
 (arrow_function
-  body: (_) @function.inner) @function.outer
+  . body: (_) @function.inner) @function.outer
+
+(arrow_function
+  body: (statement_block . "{" . (_) @_start @_end (_)? @_end . "}"
+ (#make-range! "function.inner" @_start @_end)))
 
 (method_definition
   body: (statement_block)) @function.outer

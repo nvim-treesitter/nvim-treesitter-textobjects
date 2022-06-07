e6f23225ed384da1cc66ce8f008315db9aea3447
Benny Powers <bennypowers@users.noreply.github.com>
2022-06-07 13:59:18 +0300

    feat(html): add `@attribute.inner`, `outer`
    `daa` - delete around attribute (i.e. delete entire attribute)
    `cia` - change inner attribute (i.e. attr value)

--- queries/html/textobjects.scm
+++ queries/html/textobjects.scm
@@ -2,6 +2,9 @@
 
 (element (start_tag) . (_) @function.inner . (end_tag))
 
+(attribute_value) @attribute.inner
+(attribute) @attribute.outer
+
 ((element (start_tag) . (_) @_start (_) @_end . (end_tag))
  (#make-range! "function.inner" @_start @_end))
 

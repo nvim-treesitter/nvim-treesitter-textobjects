b7150dd1d467e17e53be7f5b90d805138d68c98e
Sean Baildon <sean@baildon.co>
2022-08-12 14:49:59 -0500

    feat: add support for heex

---
+++ queries/heex/textobjects.scm
@@ -0,0 +1,9 @@
+(tag) @function.outer
+
+(tag (start_tag) . (_) @function.inner . (end_tag))
+
+(attribute_value) @attribute.inner
+(attribute) @attribute.outer
+
+((tag (start_tag) . (_) @_start (_) @_end . (end_tag))
+ (#make-range! "function.inner" @_start @_end))

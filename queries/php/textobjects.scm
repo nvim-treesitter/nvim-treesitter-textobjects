a2ed30dc874628d009ce537c6fc9db0ff91783e5
Laytan Laats <laytanlaats@hotmail.com>
2022-09-20 17:41:29 +0200

    feat: improve PHP queries
    - excludes the braces for all the inside queries
    - add query for if statements
    - add query for plain for loops

--- queries/php/textobjects.scm
+++ queries/php/textobjects.scm
@@ -1,52 +1,86 @@
-(function_definition 
-  body: (compound_statement)) @function.outer
-
+;; functions
 (function_definition
   body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
- (#make-range! "function.inner" @_start @_end)))
+  (#make-range! "function.inner" @_start @_end))) @function.outer
 
-(method_declaration
-  body: (compound_statement)) @function.outer
-
+;; methods
 (method_declaration
   body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
- (#make-range! "function.inner" @_start @_end)))
+  (#make-range! "function.inner" @_start @_end))) @function.outer
 
+;; classes
 (class_declaration
-  body: (declaration_list) @class.inner) @class.outer
+  body: (declaration_list) @class.inner)
+
+(
+  (comment) @_class_comment
+  (class_declaration) @_class_body
+  (#make-range! "class.outer" @_class_comment @_class_body)
+)
+
+;; loops
+(for_statement
+  (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))
+) @loop.outer
 
 (foreach_statement
-  body: (_)? @loop.inner) @loop.outer
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))
+) @loop.outer
 
 (while_statement
-  body: (_)? @loop.inner) @loop.outer
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))
+) @loop.outer
 
 (do_statement
-  body: (_)? @loop.inner) @loop.outer
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))
+) @loop.outer
 
+;; conditionals
 (switch_statement
-  body: (_)? @conditional.inner) @conditional.outer
+  body: (switch_block . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "conditional.inner" @_start @_end))
+) @conditional.outer
 
-;;blocks
+(if_statement
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "conditional.inner" @_start @_end))
+) @conditional.outer
+
+(else_clause
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "conditional.inner" @_start @_end))
+)
+
+(else_if_clause
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "conditional.inner" @_start @_end))
+)
+
+;; blocks
 (_ (switch_block) @block.inner) @block.outer
 
 ;; parameters
 (arguments
   "," @_start .
   (_) @parameter.inner
- (#make-range! "parameter.outer" @_start @parameter.inner))
+  (#make-range! "parameter.outer" @_start @parameter.inner))
 (arguments
   . (_) @parameter.inner
   . ","? @_end
- (#make-range! "parameter.outer" @parameter.inner @_end))
+  (#make-range! "parameter.outer" @parameter.inner @_end))
 
 (formal_parameters
   "," @_start .
   (_) @parameter.inner
- (#make-range! "parameter.outer" @_start @parameter.inner))
+  (#make-range! "parameter.outer" @_start @parameter.inner))
 (formal_parameters
   . (_) @parameter.inner
   . ","? @_end
- (#make-range! "parameter.outer" @parameter.inner @_end))
+  (#make-range! "parameter.outer" @parameter.inner @_end))
 
+;; comments
 (comment) @comment.outer

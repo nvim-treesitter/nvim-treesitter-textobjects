fa5e5a525c4a8ce48209877da2f676f292fda6f4
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-19 18:18:46 +0100

    feat: add haskell loop queries
    * loop.inner
    * loop.outer

--- queries/haskell/textobjects.scm
+++ queries/haskell/textobjects.scm
@@ -19,3 +19,23 @@
   (_) @conditional.inner
   ) 
 
+;; e.g. forM [1..10] $ \i -> do...
+(exp_infix
+  (exp_apply
+    (exp_name)@_name
+    (#any-of? @_name "for" "forM" "forM")
+  ) 
+  (operator) @_op
+  (#eq? @_op "$")
+  (exp_lambda
+    (_)
+    (_) @loop.inner
+  )
+) @loop.outer
+;; e.g. forM [1..10] print
+(exp_apply
+  (exp_name)@_name
+  (#any-of? @_name "for" "forM" "forM")
+  (_)
+  (_) @loop.inner
+) @loop.outer

1cf7956f93b5cc888ae6c30a0c80756bc1a9f102
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-22 20:04:39 +0100

    feat: add some queries for nix

---
+++ queries/nix/textobjects.scm
@@ -0,0 +1,27 @@
+;; named function
+(binding
+  (function_expression
+  ) 
+) @function.outer
+
+;; anonymous function
+(function_expression
+  (_) ;; argument
+  (_) @function.inner
+) @function.outer
+
+(function_expression
+  (formals
+    (formal) @parameter.inner
+  ) 
+)
+(function_expression
+  (_) @parameter.outer
+  (_)
+)
+
+(comment) @comment.outer
+
+(if_expression
+  (_) @conditional.inner
+) @conditional.outer

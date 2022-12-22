3d785e58e8ebfc286c350f241a2ef6817dd1bafd
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-22 19:16:35 +0000

    preparing python tests: working event demo

---
+++ scripts/python_tests/README.md
@@ -0,0 +1,5 @@
+# Nvim_communicator Python Package
+
+This project is made for testing neovim plugins, specifically focussing on Nvim-treesitter-textobjects for now.
+
+It uses pynvim to communicate with neovim, executing lua scripts or input keys and then return the results.
---
+++ scripts/python_tests/lua/autocmd_visualenter.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+  buffer = vim.g.nvim_communicator_buffer_id,
+  callback = function()
+    local old_mode = vim.api.nvim_get_vvar('event')['old_mode']
+    local new_mode = vim.api.nvim_get_vvar('event')['new_mode']
+    print(new_mode)
+    if new_mode == 'V' or new_mode == 'v' or new_mode == '\x16' then
+      Nvim_communicator_rpcnotify("visual_enter", old_mode, new_mode)
+    end
+  end
+})

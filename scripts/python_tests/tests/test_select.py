89160e4ef8a1275c378a3238ebdc6d525c4bfc83
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-25 23:11:13 +0000

    fix test and github action

--- scripts/python_tests/tests/test_select.py
+++ scripts/python_tests/tests/test_select.py
@@ -1,4 +1,4 @@
-from nvim_communicator import receive_all_pending_messages
+from nvim_communicator import receive_all_pending_messages, events_to_listdict
 
 
 def test_select_function(nvim):
@@ -6,9 +6,22 @@ def test_select_function(nvim):
     nvim.command("normal vam")
     nvim.feedkeys(nvim.replace_termcodes("<esc>", True, True, True))
     events = receive_all_pending_messages(nvim)
+    events = events_to_listdict(events)
     for event in events:
-        assert event[1] != "on_bytes"
-        assert event[1] != "on_bytes_removed"
+        assert event["name"] != "on_bytes"
+        assert event["name"] != "on_bytes_removed"
 
-    assert events[-1][1] == "visual_leave"
-    assert events[-1][2] == ["v", "n", 19, 4, 24, 37]
+    assert events[-2]["name"] == "visual_leave"
+    assert events[-2]["args"] == {
+        "old_mode": "v",
+        "new_mode": "n",
+        "start_row": 19,
+        "start_col": 4,
+        "end_row": 24,
+        "end_col": 37,
+    }
+    assert events[-1]["name"] == "CursorMoved"
+    assert events[-1]["args"] == {
+        "cursor_pos_row": 24,
+        "cursor_pos_col": 37,
+    }

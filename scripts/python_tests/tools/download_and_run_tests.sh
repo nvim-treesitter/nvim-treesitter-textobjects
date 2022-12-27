8e35042fbed593fe63ff85cae76fcbeff43df4b4
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-27 23:41:10 +0000

    consistency test fix error not failing CI

--- scripts/python_tests/tools/download_and_run_tests.sh
+++ scripts/python_tests/tools/download_and_run_tests.sh
@@ -11,8 +11,14 @@ tar xvfz "$TEMPDIR/$FILE" -C "$TEMPDIR" &> /dev/null
 
 tests_path=$(ls "$TEMPDIR"/*.yaml)
 
+exit_code=0
 while read -r test_yaml_path; do
 	python "$SOURCE_DIR/run_test_yaml.py" "$test_yaml_path"
+	if [ $? -ne 0 ]; then
+		exit_code=1
+	fi
 done <<< "$tests_path"
 
 rm -rf "$TEMPDIR"
+
+exit $exit_code

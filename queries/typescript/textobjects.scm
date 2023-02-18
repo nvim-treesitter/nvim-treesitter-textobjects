73c20e59c62e5b3099cb75c3676e6e0b363915f0
benyn <105991837+benyn@users.noreply.github.com>
2023-02-18 12:46:56 -0800

    Update TypeScript textobjects for type definition
    `interface`, `type` and `enum` declarations are now supported as class `textobject`s. Tested and confirmed that trailing semicolons (`;`) and commas (`,`) are captured in `@class.inner`, if present.

--- queries/typescript/textobjects.scm
+++ queries/typescript/textobjects.scm
@@ -1,32 +1,25 @@
 ; inherits: ecma
 
-
 ;; interface declaration as class textobject
-(interface_declaration) @class.outer
+(interface_declaration
+  body: (object_type)) @class.outer
 
 (interface_declaration
-  body: _ @class.inner) @class.outer
-
-(interface_declaration
-  body: (object_type . "{" . _ @_start @_end _? @_end . "}"
+  body: (object_type . "{" . (_) @_start @_end _? @_end . "}"
  (#make-range! "class.inner" @_start @_end)))
 
 ;; type alias declaration as class textobject
-(type_alias_declaration) @class.outer
+(type_alias_declaration
+  value: (object_type)) @class.outer
 
 (type_alias_declaration
-  value: _ @class.inner) @class.outer
-
-(type_alias_declaration
-  value: (object_type . "{" . _ @_start @_end _? @_end . "}"
+  value: (object_type . "{" . (_) @_start @_end _? @_end . "}"
  (#make-range! "class.inner" @_start @_end)))
 
-;; enum declaration as class text object
-(enum_declaration) @class.outer
+;; enum declaration as class textobject
+(enum_declaration
+  body: (enum_body)) @class.outer
 
 (enum_declaration
-  body: (enum_body (_) @class.inner)) @class.outer
-
-(enum_declaration
-  body: (enum_body . "{" . _ @_start @_end _? @_end . "}"
+  body: (enum_body . "{" . (_) @_start @_end _? @_end . "}"
  (#make-range! "class.inner" @_start @_end)))

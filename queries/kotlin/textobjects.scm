a4e059d5252851bcd9e857f640d7192e9639e11a
Thore Strassburg <thore@weilbier.net>
2023-03-04 14:17:32 +0100

    Feat(kotlin): add bunch of new text objects
    The list of supported text objects for the language Kotlin were
    absolutely minimal so far. To improve the language support, this adds
    the most common objects.
    But neither are all possible text objects supported yet, nor are all
    possible trees covered. Though it should be a fairly good start to work
    with.

--- queries/kotlin/textobjects.scm
+++ queries/kotlin/textobjects.scm
@@ -1,3 +1,35 @@
+(class_declaration
+  [
+    (class_body)
+    (enum_class_body)
+  ] @class.inner
+) @class.outer
+
+[
+  (function_declaration (function_body) @function.inner)
+  (getter (function_body) @function.inner)
+  (setter (function_body) @function.inner)
+  (primary_constructor)
+] @function.outer
+
+(primary_constructor) @function.inner
+
+[
+  (parameter (simple_identifier) @parameter.inner)
+  (class_parameter (simple_identifier) @parameter.inner) 
+] @parameter.outer
+
+(comment) @comment.outer
+
+(if_expression (control_structure_body) @conditional.inner) @conditional.outer
+
+(when_expression (when_entry) @conditional.inner) @conditional.outer
+
+[
+  (for_statement (control_structure_body) @loop.inner)
+  (while_statement (control_structure_body) @loop.inner)
+] @loop.outer
+
 [
   (integer_literal)
   (real_literal)

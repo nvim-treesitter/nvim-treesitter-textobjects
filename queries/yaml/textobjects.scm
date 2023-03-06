98c66344d2f25a3976a81d76256f0027327abadc
Yochem van Rosmalen <yochem+git@icloud.com>
2023-03-06 15:49:20 +0100

    yaml: add assignment, statement and comment

--- queries/yaml/textobjects.scm
+++ queries/yaml/textobjects.scm
@@ -1,4 +1,27 @@
+;; assignment, statement
+(block_mapping_pair
+  key: (_) @assignment.lhs
+  value: (_) @assignment.rhs) @assignment.outer @statement.outer
+
+(block_mapping_pair
+  key: (_) @assignment.inner)
+
+(block_mapping_pair
+  value: (_) @assignment.inner)
+
+;; comment
+; leave space after comment marker if there is one
+((comment) @comment.inner @comment.outer
+           (#offset! @comment.inner 0 2 0)
+           (#lua-match? @comment.outer "# .*"))
+
+; else remove everything accept comment marker
+((comment) @comment.inner @comment.outer
+  (#offset! @comment.inner 0 1 0))
+
+;; number
 [
   (integer_scalar)
   (float_scalar)
 ] @number.inner
+

f5f13206ec33e55b16c8e1a9dec301e8bead4835
Yochem van Rosmalen <yochem+git@icloud.com>
2023-03-06 23:40:03 +0100

    v: Add V textobjects (#404)
    * v: Add V textobjects
    
    The following textobjects are added:
    
    - [x] @assignment.inner
    - [x] @assignment.lhs
    - [x] @assignment.outer
    - [x] @assignment.rhs
    - [ ] @attribute.inner
    - [ ] @attribute.outer
    - [x] @block.inner
    - [x] @block.outer
    - [x] @call.inner
    - [x] @call.outer
    - [x] @class.inner
    - [x] @class.outer
    - [x] @comment.inner
    - [x] @comment.outer
    - [x] @conditional.inner
    - [x] @conditional.outer
    - [ ] @frame.inner
    - [ ] @frame.outer
    - [x] @function.inner
    - [x] @function.outer
    - [x] @loop.inner
    - [x] @loop.outer
    - [ ] @number.inner
    - [x] @parameter.inner
    - [x] @parameter.outer
    - [ ] @regex.inner
    - [ ] @regex.outer
    - [x] @return.inner
    - [x] @return.outer
    - [ ] @scopename.inner
    - [x] @statement.outer
    
    * v: add `@assignment.inner`
    
    * v: `@comment.inner` detects leading space
    
    Example using change inner comment (`cic`):
    
    Without leading space:
    ```v
    //test
    ```
    Becomes (`|` denotes cursor position):
    ```v
    //|
    ```
    
    With leading space:
    ```v
    // test
    ```
    Becomes (`|` denotes cursor position):
    ```v
    // |
    ```
    
    * v: add `@number.inner`
    
    * v: don't let `@block.inner` consume {}

---
+++ queries/v/textobjects.scm
@@ -0,0 +1,83 @@
+;; assignment
+[(short_var_declaration
+    left: (_) @assignment.lhs
+    right: (_)* @assignment.rhs)
+  (assignment_statement
+    left: (_) @assignment.lhs
+    right: (_)* @assignment.rhs)]
+
+[(short_var_declaration
+    left: (_) @assignment.inner)
+  (assignment_statement
+    left: (_) @assignment.inner)]
+
+[(short_var_declaration
+    right: (_) @assignment.inner)
+  (assignment_statement
+    right: (_) @assignment.inner)]
+
+;; block
+(_ (block . "{" . (_) @_start @_end (_)? @_end . "}")
+   (#make-range! "block.inner" @_start @_end)) @block.outer
+
+;; call
+(call_expression) @call.outer
+(call_expression
+  arguments: (argument_list . "(" . (_) @_start (_)? @_end . ")"
+  (#make-range! "call.inner" @_start @_end)))
+
+;; class: structs
+(struct_declaration
+  (struct_field_declaration_list . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "class.inner" @_start @_end)))
+
+(struct_declaration) @class.outer
+
+;; comment
+; leave space after comment marker if there is one
+((comment) @comment.inner @comment.outer
+           (#offset! @comment.inner 0 3 0)
+           (#lua-match? @comment.outer "// .*"))
+
+; else remove everything accept comment marker
+((comment) @comment.inner @comment.outer
+  (#offset! @comment.inner 0 2 0))
+
+;; conditional
+(if_expression
+  consequence: (block . "{" . (_) @_start @_end (_)? @_end . "}"
+    (#make-range! "conditional.inner" @_start @_end))?) @conditional.outer
+
+;; function
+(function_declaration
+  body: (block . "{" . (_) @_start @_end (_)? @_end . "}"
+ (#make-range! "function.inner" @_start @_end)))
+
+(function_declaration) @function.outer
+
+;; loop
+(for_statement
+  body: (block . "{" . (_) @_start @_end (_)? @_end . "}"
+    (#make-range! "loop.inner" @_start @_end))?) @loop.outer
+
+[
+  (int_literal)
+  (float_literal)
+] @number.inner
+
+;; parameter
+(parameter_list
+  "," @_start .
+  (parameter_declaration) @parameter.inner
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+
+(parameter_list
+  . (parameter_declaration) @parameter.inner
+  . ","? " "? @_end
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+;; return
+(return_statement (_)* @return.inner) @return.outer
+
+;; statements
+(block (_) @statement.outer)

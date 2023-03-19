39d8bf03a1c193eb6544da4e22515e03ce1fca29
benyn <105991837+benyn@users.noreply.github.com>
2023-03-19 12:19:52 -0700

    feat(typescript): interface, type and enum as class textobj (#398)
    * Update TypeScript textobjects for type definition support
    
    `interface`, `type` and `enum` declarations are now supported as class `textobject`s.
    
    Known issues:
    - the last semicolon (`;`) of the inner text (`interface`, `type`) or the last comma of the inner text (`enum`) are missed.
    - an object [declared with `as const`](https://www.typescriptlang.org/docs/handbook/enums.html#objects-vs-enums) isn't supported (`value: as_expression` ending with `type_identifier`)

--- queries/typescript/textobjects.scm
+++ queries/typescript/textobjects.scm
@@ -1,1 +1,25 @@
 ; inherits: ecma
+
+;; interface declaration as class textobject
+(interface_declaration
+  body: (object_type)) @class.outer
+
+(interface_declaration
+  body: (object_type . "{" . (_) @_start @_end _? @_end . "}"
+ (#make-range! "class.inner" @_start @_end)))
+
+;; type alias declaration as class textobject
+(type_alias_declaration
+  value: (object_type)) @class.outer
+
+(type_alias_declaration
+  value: (object_type . "{" . (_) @_start @_end _? @_end . "}"
+ (#make-range! "class.inner" @_start @_end)))
+
+;; enum declaration as class textobject
+(enum_declaration
+  body: (enum_body)) @class.outer
+
+(enum_declaration
+  body: (enum_body . "{" . (_) @_start @_end _? @_end . "}"
+ (#make-range! "class.inner" @_start @_end)))

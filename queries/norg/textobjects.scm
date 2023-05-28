a0bb195522263592edbbe7d32972315dc506d267
Am Laher <am@laher.net.nz>
2023-05-28 21:33:00 +1200

    queries: add support for norg (with query copied from neorg's own folds.scm)

---
+++ queries/norg/textobjects.scm
@@ -0,0 +1,13 @@
+(ranged_verbatim_tag
+    name: (tag_name) @_name
+    (#eq? @_name "document.meta")
+) @fold
+
+[
+    (heading1)
+    (heading2)
+    (heading3)
+    (heading4)
+    (heading5)
+    (heading6)
+] @fold

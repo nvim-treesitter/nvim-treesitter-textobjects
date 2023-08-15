ab21cd184674be552182e95f86aab1e3d1278791
ObserverOfTime <chronobserver@disroot.org>
2023-08-15 11:49:05 +0300

    feat(perl)!: update textobjects

--- queries/perl/textobjects.scm
+++ queries/perl/textobjects.scm
@@ -1,13 +1,5 @@
-(comments) @comment.outer
+(comment) @comment.outer
 
-(pattern_matcher (regex_pattern) @regex.inner) @regex.outer
+(quoted_regexp) @regex.outer
 
-((patter_matcher_m
-   (start_delimiter) @_start
-   (end_delimiter) @_end) @regex.outer
- (#make-range! "regex.inner" @_start @_end))
-
-((regex_pattern_qr
-   (start_delimiter) @_start
-   (end_delimiter) @_end) @regex.outer
- (#make-range! "regex.inner" @_start @_end))
+(match_regexp) @regex.outer

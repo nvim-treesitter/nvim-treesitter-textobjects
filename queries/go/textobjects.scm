fb693331bc28f01bad429bf43f2c316c8908a78b
Nik Klassen <nikklassen@users.noreply.github.com>
2023-10-01 10:47:23 -0400

    Add assignment textobjects for Go

--- queries/go/textobjects.scm
+++ queries/go/textobjects.scm
@@ -97,3 +97,25 @@
   . (_) @parameter.inner
   . ","? @_end
  (#make-range! "parameter.outer" @parameter.inner @_end))
+
+;; assignments
+(short_var_declaration
+  left: (_) @assignment.lhs
+  right: (_) @assignment.rhs @assignment.inner) @assignment.outer
+(assignment_statement
+  left: (_) @assignment.lhs
+  right: (_) @assignment.rhs @assignment.inner) @assignment.outer
+(var_spec
+  name: (_) @assignment.lhs
+  value: (_) @assignment.rhs @assignment.inner)
+(var_spec
+  name: (_) @assignment.inner
+  type: (_))
+(var_spec) @assignment.outer
+(const_spec
+  name: (_) @assignment.lhs
+  value: (_) @assignment.rhs @assignment.inner)
+(const_spec
+  name: (_) @assignment.inner
+  type: (_))
+(const_spec) @assignment.outer

a1785862865e121404e4e97d395dc676cba21a91
mortezadadgar <mortezadadgar97@gmail.com>
2023-11-07 13:56:40 +0330

    fix(c): exclude curly braces for inner selection

--- queries/c/textobjects.scm
+++ queries/c/textobjects.scm
@@ -19,23 +19,32 @@
 (enum_specifier
   body: (_) @class.inner) @class.outer
 
-; conditional
+; conditionals
 (if_statement
-  consequence: (_)? @conditional.inner
-  alternative: (_)? @conditional.inner
-  ) @conditional.outer
+  consequence: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "conditional.inner" @_start @_end))) @conditional.outer
 
 (if_statement
-  condition: (_) @conditional.inner)
+  alternative: (else_clause (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "conditional.inner" @_start @_end)))) @conditional.outer
+
+(if_statement) @conditional.outer
 
 ; loops
-(for_statement
-  (_)? @loop.inner) @loop.outer
+(while_statement) @loop.outer
 (while_statement
-  (_)? @loop.inner) @loop.outer
-(do_statement
-  (_)? @loop.inner) @loop.outer
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))) @loop.outer
 
+(for_statement) @loop.outer
+(for_statement
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))) @loop.outer
+
+(do_statement) @loop.outer
+(do_statement
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+  (#make-range! "loop.inner" @_start @_end))) @loop.outer
 
 (compound_statement) @block.outer
 (comment) @comment.outer

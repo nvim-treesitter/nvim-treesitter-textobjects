c737575efcf2e9705149e3aaaf7c7c758e8adb64
hungpt-a8e <49029378+hungpt-a8e@users.noreply.github.com>
2023-11-13 05:25:43 +0700

    feat(gdscript): add textobjects

---
+++ queries/gdscript/textobjects.scm
@@ -0,0 +1,58 @@
+(function_definition
+  body: (_) @function.inner) @function.outer
+
+(constructor_definition
+  body: (_) @function.inner) @function.outer
+
+(class_definition
+  body: (_) @class.inner) @class.outer
+
+(if_statement
+  body: (_) @conditional.inner) @conditional.outer
+
+(if_statement
+  alternative: (_ (_) @conditional.inner)?) @conditional.outer
+
+(if_statement
+  condition: (_) @conditional.inner)
+
+[
+  (for_statement)
+  (while_statement)
+] @loop.outer
+
+(while_statement body: (_) @loop.inner)
+
+(for_statement body: (_) @loop.inner)
+
+(comment) @comment.outer
+
+(parameters
+  "," @_start .
+  (_) @parameter.inner
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+
+(parameters
+  . (_) @parameter.inner
+  . ","? @_end
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+(arguments
+  "," @_start .
+  (_) @parameter.inner
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+
+(arguments
+  . (_) @parameter.inner
+  . ","? @_end
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+(array
+  "," @_start .
+  (_) @parameter.inner
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+
+(array
+  . (_) @parameter.inner
+  . ","? @_end
+ (#make-range! "parameter.outer" @parameter.inner @_end))

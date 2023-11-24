a44ba552702ba72daf7c58cbf83e53cb2e72bb0a
TheLeoP <eugenio2305@hotmail.com>
2023-11-24 15:44:45 -0600

    feat: use nvim-treesitter main branch on minimal_init.lua

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -5,12 +5,9 @@ packadd plenary.nvim
 TSUpdate
 ]]
 
-require("nvim-treesitter.configs").setup {
+require("nvim-treesitter").setup {
   -- A list of parser names, or "all"
-  ensure_installed = "python",
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
+  ensure_installed = { "python" },
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -19,17 +16,6 @@ require("nvim-treesitter.configs").setup {
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
   -- parser_install_dir = "/some/path/to/store/parsers",
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = true,
-  },
 }
 
 local select = require "nvim-treesitter-textobjects.select"

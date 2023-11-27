588fc8c365e89f0c68a10ae123c5149518c02fb5
TheLeoP <eugenio2305@hotmail.com>
2023-11-27 00:24:55 -0600

    refactor!: remove unused options from config

--- lua/nvim-treesitter-textobjects/config.lua
+++ lua/nvim-treesitter-textobjects/config.lua
@@ -5,39 +5,25 @@
 ---@class (exact) TSTextObjects.Config.Select
 ---@field lookahead boolean
 ---@field lookbehind boolean
----@field keymaps table<string, string|table> TODO (TheLeoP): remove?
 ---@field selection_modes table<string, TSTextObjects.SelectionMode>|fun(opts: {query_string: string, method: TSTextObjects.Method}): TSTextObjects.SelectionMode|table<string, TSTextObjects.SelectionMode>
 ---@field include_surrounding_whitespace boolean|fun(): boolean
 
 -- luacheck: pop
 
----@class (exact) TSTextObjects.Config.Swap
----@field swap_next table<string, string> TODO (TheLeoP): remove?
----@field swap_previous table<string, string> TODO (TheLeoP): remove?
-
 ---@class (exact) TSTextObjects.Config.Move
 ---@field set_jumps boolean
----@field goto_next_start table<string, string> TODO (TheLeoP): remove?
----@field goto_next_end table<string, string> TODO (TheLeoP): remove?
----@field goto_previous_start table<string, string> TODO (TheLeoP): remove?
----@field goto_previous_end table<string, string> TODO (TheLeoP): remove?
----@field goto_previous table<string, string> TODO (TheLeoP): remove?
----@field goto_next table<string, string> TODO (TheLeoP): remove?
 
 ---@class (exact) TSTextObjects.Config.LspInterop
 ---@field border string|string[]
 ---@field floating_preview_opts table
----@field peek_definition_code table<string, string> TODO (TheLeoP): remove?
 
 ---@class (exact) TSTextObjects.Config
 ---@field select TSTextObjects.Config.Select
----@field swap TSTextObjects.Config.Swap
 ---@field move TSTextObjects.Config.Move
 ---@field lsp_interop TSTextObjects.Config.LspInterop
 
 ---@class (exact) TSTextObjects.UserConfig : TSTextObjects.Config
 ---@field select TSTextObjects.Config.Select
----@field swap TSTextObjects.Config.Swap
 ---@field move TSTextObjects.Config.Move
 ---@field lsp_interop TSTextObjects.Config.LspInterop
 
@@ -46,27 +32,15 @@ local default_config = {
   select = {
     lookahead = false,
     lookbehind = false,
-    keymaps = {},
     selection_modes = {},
     include_surrounding_whitespace = false,
   },
-  swap = {
-    swap_next = {},
-    swap_previous = {},
-  },
   move = {
     set_jumps = true,
-    goto_next_start = {},
-    goto_next_end = {},
-    goto_previous_start = {},
-    goto_previous_end = {},
-    goto_next = {},
-    goto_previous = {},
   },
   lsp_interop = {
     border = "none",
     floating_preview_opts = {},
-    peek_definition_code = {},
   },
 }
 

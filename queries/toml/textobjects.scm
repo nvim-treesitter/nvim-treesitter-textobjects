a05979b623b5017043c20e17ba949fb279d0eb60
Ankit Goel <ankitgoel616@gmail.com>
2024-02-08 00:46:19 +0000

    feat(toml): Add @parameter.inner and @parameter.outer (#561)
    Includes array elements, key-val pair in inline tables and key-val pairs
    in normal tables.

--- queries/toml/textobjects.scm
+++ queries/toml/textobjects.scm
@@ -2,3 +2,31 @@
   (integer)
   (float)
 ] @number.inner
+
+((table
+    (pair) @parameter.inner @parameter.outer
+  ))
+
+((inline_table
+    "," @_start .
+    (_) @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+((inline_table
+    . (_) @parameter.inner
+    . ","? @_end
+  )
+  (#make-range! "parameter.outer" @parameter.inner @_end))
+
+((array
+    "," @_start .
+    (_) @parameter.inner
+  )
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+((array
+    . (_) @parameter.inner
+    . ","? @_end
+  )
+  (#make-range! "parameter.outer" @parameter.inner @_end))

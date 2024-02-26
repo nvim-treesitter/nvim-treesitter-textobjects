ebc1fe8c4c0980ab11a968ee966683f1aa42ca4f
wessel.blokzijl <wessel.blokzijl@huntandhackett.com>
2024-02-26 14:30:43 +0100

    Add hcl queries

--- queries/hcl/textobjects.scm
+++ queries/hcl/textobjects.scm
@@ -1,8 +1,36 @@
-(block
-  (body)? @block.inner) @block.outer
+(attribute
+ (identifier) @assignment.lhs
+ (expression) @assignment.inner @assignment.rhs) @assignment.outer
+(attribute
+ (identifier) @assignment.inner)
 
-(block
-  (body
-    (_) @statement.outer))
+(block (body)? @block.inner ) @block.outer
+(block (body (_) @statement.outer))
+
+(function_call (function_arguments) @call.inner) @call.outer
 
 (comment) @comment.outer
+
+(conditional
+ (expression) @conditional.condition
+ (expression) @conditional.lhs
+ (expression) @conditional.inner @conditional.rhs) @conditional.outer
+(conditional
+ (expression) @conditional.inner)
+
+(for_expr (for_object_expr (for_intro) @loop.control (expression) @loop.lhs (expression) @loop.rhs (for_cond)? @loop.condition @loop.inner )) @loop.outer
+(for_expr (for_object_expr (for_intro) @loop.inner ))
+(for_expr (for_object_expr (expression) @loop.inner ))
+(for_expr (for_tuple_expr (for_intro) @loop.control (expression) @loop.lhs @loop.rhs  (for_cond)? @loop.condition @loop.inner )) @loop.outer
+(for_expr (for_tuple_expr (for_intro) @loop.inner ))
+(for_expr (for_tuple_expr (expression) @loop.inner ))
+
+(numeric_lit) @number.inner
+
+((function_arguments
+  "," @_start . (expression) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((function_arguments
+  . (expression) @parameter.inner . ","? @_end)
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+

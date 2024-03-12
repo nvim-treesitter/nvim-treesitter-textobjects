f0375e1bdc1b245ae5a7f71272bec17053b1163f
Rosen Stoyanov <pockata@users.noreply.github.com>
2024-03-12 14:53:04 +0200

    feat(css): add assignment textobjects (#544)

--- queries/css/textobjects.scm
+++ queries/css/textobjects.scm
@@ -3,3 +3,14 @@
   (float_value)
   (color_value)
 ] @number.inner
+
+(declaration
+  (property_name) @assignment.lhs
+  . ":" . (_) @_start (_)? @_end . ";"
+  (#make-range! "assignment.inner" @_start @_end)
+  (#make-range! "assignment.rhs" @_start @_end)
+) @assignment.outer
+
+(declaration
+  (property_name) @assignment.inner
+)

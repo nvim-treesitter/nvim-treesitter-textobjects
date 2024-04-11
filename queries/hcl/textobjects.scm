20ff89858005876493fe111d528b248b89e2791a
wessel.blokzijl <wessel.blokzijl@huntandhackett.com>
2024-04-11 05:12:57 +0200

    Run formatter

--- queries/hcl/textobjects.scm
+++ queries/hcl/textobjects.scm
@@ -1,36 +1,70 @@
 (attribute
- (identifier) @assignment.lhs
- (expression) @assignment.inner @assignment.rhs) @assignment.outer
+  (identifier) @assignment.lhs
+  (expression) @assignment.inner @assignment.rhs) @assignment.outer
+
 (attribute
- (identifier) @assignment.inner)
+  (identifier) @assignment.inner)
 
-(block (body)? @block.inner ) @block.outer
-(block (body (_) @statement.outer))
+(block
+  (body)? @block.inner) @block.outer
 
-(function_call (function_arguments) @call.inner) @call.outer
+(block
+  (body
+    (_) @statement.outer))
+
+(function_call
+  (function_arguments) @call.inner) @call.outer
 
 (comment) @comment.outer
 
 (conditional
- (expression) @conditional.condition
- (expression) @conditional.lhs
- (expression) @conditional.inner @conditional.rhs) @conditional.outer
-(conditional
- (expression) @conditional.inner)
+  (expression) @conditional.condition
+  (expression) @conditional.lhs
+  (expression) @conditional.inner @conditional.rhs) @conditional.outer
 
-(for_expr (for_object_expr (for_intro) @loop.control (expression) @loop.lhs (expression) @loop.rhs (for_cond)? @loop.condition @loop.inner )) @loop.outer
-(for_expr (for_object_expr (for_intro) @loop.inner ))
-(for_expr (for_object_expr (expression) @loop.inner ))
-(for_expr (for_tuple_expr (for_intro) @loop.control (expression) @loop.lhs @loop.rhs  (for_cond)? @loop.condition @loop.inner )) @loop.outer
-(for_expr (for_tuple_expr (for_intro) @loop.inner ))
-(for_expr (for_tuple_expr (expression) @loop.inner ))
+(conditional
+  (expression) @conditional.inner)
+
+(for_expr
+  (for_object_expr
+    (for_intro) @loop.control
+    (expression) @loop.lhs
+    (expression) @loop.rhs
+    (for_cond)? @loop.condition @loop.inner)) @loop.outer
+
+(for_expr
+  (for_object_expr
+    (for_intro) @loop.inner))
+
+(for_expr
+  (for_object_expr
+    (expression) @loop.inner))
+
+(for_expr
+  (for_tuple_expr
+    (for_intro) @loop.control
+    (expression) @loop.lhs @loop.rhs
+    (for_cond)? @loop.condition @loop.inner)) @loop.outer
+
+(for_expr
+  (for_tuple_expr
+    (for_intro) @loop.inner))
+
+(for_expr
+  (for_tuple_expr
+    (expression) @loop.inner))
 
 (numeric_lit) @number.inner
 
 ((function_arguments
-  "," @_start . (expression) @parameter.inner)
- (#make-range! "parameter.outer" @_start @parameter.inner))
-((function_arguments
-  . (expression) @parameter.inner . ","? @_end)
- (#make-range! "parameter.outer" @parameter.inner @_end))
+  "," @_start
+  .
+  (expression) @parameter.inner)
+  (#make-range! "parameter.outer" @_start @parameter.inner))
 
+((function_arguments
+  .
+  (expression) @parameter.inner
+  .
+  ","? @_end)
+  (#make-range! "parameter.outer" @parameter.inner @_end))

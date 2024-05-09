dfa4178c0cadb44f687603d72ad0908474c28dd9
Alexei Mozaidze <lexerpexer@proton.me>
2024-05-09 21:48:29 +0400

    feat(fennel): update queries for new parser (#606)
    * feat(fennel): update queries for new parser
    
    Updated queries to support the new parser merged with nvim-treesitter/nvim-treesitter#6460.
    
    * fix(fennel): improve queries

--- queries/fennel/textobjects.scm
+++ queries/fennel/textobjects.scm
@@ -1,41 +1,215 @@
 ; https://fennel-lang.org/reference
-(comment) @comment.outer
+(comment
+  body: (comment_body) @comment.inner) @comment.outer
 
 (_
   .
   "("
   ")" .) @statement.outer
 
-; functions & parameters
-(list
-  .
-  (symbol) @_fn
-  (symbol)? @_fn_name
-  (sequence
-    (_) @parameter.inner)
-  (_)* @function.inner
-  (#any-of? @_fn "fn" "lambda" "λ")) @function.outer
+; functions
+; NOTE: Doesn't capture the comments before the first `item` field
+([
+  (fn_form
+    [
+      (table_metadata)
+      (docstring)
+    ]
+    .
+    item: (_) @_start
+    (_)? @_end
+    .
+    close: _ .)
+  (fn_form
+    args: (_)
+    .
+    item: (_) @_start
+    (_)? @_end
+    .
+    close: _ .)
+  (lambda_form
+    [
+      (table_metadata)
+      (docstring)
+    ]
+    .
+    item: (_) @_start
+    (_)? @_end
+    .
+    close: _ .)
+  (lambda_form
+    args: (_)
+    .
+    item: (_) @_start
+    (_)? @_end
+    .
+    close: _ .)
+  (macro_form
+    [
+      (table_metadata)
+      (docstring)
+    ]
+    .
+    item: (_) @_start
+    (_)? @_end
+    .
+    close: _ .)
+  (macro_form
+    args: (_)
+    .
+    item: (_) @_start
+    (_)? @_end
+    .
+    close: _ .)
+]
+  (#make-range! "function.inner" @_start @_end))
+
+[
+  (fn_form)
+  (lambda_form)
+  (macro_form)
+] @function.outer
+
+; function arguments
+(sequence_arguments
+  item: (_) @parameter.inner) @parameter.outer
 
 ; call
 (list
+  call: (symbol) @_fn_name
+  item: (_) @_start
+  (_) @_end
   .
-  (symbol) @_fn_name
-  (_)* @call.inner
-  (#not-any-of? @_fn_name "if" "do" "while" "for" "each" "let" "when" "fn")) @call.outer
+  close: _
+  (#not-any-of? @_fn_name "do" "while" "when")
+  (#make-range! "call.inner" @_start @_end)) @call.outer
+
+; assignment
+(local_form
+  (binding_pair
+    lhs: (_) @assignment.lhs
+    rhs: (_) @assignment.rhs) @assignment.inner) @assignment.outer
+
+(var_form
+  (binding_pair
+    lhs: (_) @assignment.lhs
+    rhs: (_) @assignment.rhs) @assignment.inner) @assignment.outer
+
+(global_form
+  (binding_pair
+    lhs: (_) @assignment.lhs
+    rhs: (_) @assignment.rhs) @assignment.inner) @assignment.outer
+
+(set_form
+  (binding_pair
+    lhs: (_) @assignment.lhs
+    rhs: (_) @assignment.rhs) @assignment.inner) @assignment.outer
+
+(let_vars
+  (binding_pair
+    lhs: (_) @assignment.lhs
+    rhs: (_) @assignment.rhs) @assignment.inner) @assignment.outer
 
 ; conditionals
+(if_form
+  (if_pair
+    expression: (_) @conditional.inner)) @conditional.outer
+
+(if_form
+  else: (_) @conditional.inner) @conditional.outer
+
 (list
-  (symbol) @_cond
-  (_)
-  (_)* @conditional.inner
-  (#any-of? @_cond "if" "when")) @conditional.outer
+  call: (symbol) @_cond
+  .
+  item: (_)
+  item: (_)* @conditional.inner
+  (#eq? @_cond "when")) @conditional.outer
 
 ; loops
+(each_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(each_form) @loop.outer
+
+(collect_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(collect_form) @loop.outer
+
+(icollect_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(icollect_form) @loop.outer
+
+(accumulate_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(accumulate_form) @loop.outer
+
+(for_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(for_form) @loop.outer
+
+(fcollect_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(fcollect_form) @loop.outer
+
+(faccumulate_form
+  iter_body: (_)
+  .
+  (_) @_start
+  (_)? @_end
+  .
+  close: _
+  (#make-range! "loop.inner" @_start @_end))
+
+(faccumulate_form) @loop.outer
+
 (list
+  call: (symbol) @_sym
   .
-  (symbol) @_sym
-  .
-  (_)
-  .
-  (_)* @loop.inner
-  (#any-of? @_sym "each" "while" "for")) @loop.outer
+  item: (_)
+  item: (_)* @loop.inner
+  (#any-of? @_sym "while"))
+
+(list
+  call: (symbol) @_sym
+  (#eq? @_sym "while")) @loop.outer

8c70e02a7ec35d08e28c061411d05d695f51d1a8
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-06-19 19:56:24 +0200

    feat(c_sharp): add class.outer support for records, enums, interfaces
    Extends support for recognizing class.outer for the following types:
    
    - classes with empty declarations
    
    ```cs
    public class MyClass {}
    ```
    
    - structs with empty declarations
    
    ```cs
    public struct MyStruct {}
    ```
    
    - records
    
    ```cs
    public record MyRecord1();
    public record MyRecord2() {}
    public record MyRecord3 {}
    ```
    
    - interfaces
    
    ```cs
    public IMyInterface {}
    ```
    
    - enums
    
    ```cs
    public enum MyEnum {}
    ```
    
    Before these changes, only structs and classes (both needed to have at least 1 member) had been supported.

--- queries/c_sharp/textobjects.scm
+++ queries/c_sharp/textobjects.scm
@@ -9,6 +9,14 @@
     "}"
     (#make-range! "class.inner" @_start @_end))) @class.outer
 
+(class_declaration
+  body: (declaration_list
+    .
+    "{"
+    .
+    "}"
+    )) @class.outer
+
 (struct_declaration
   body: (declaration_list
     .
@@ -19,6 +27,71 @@
     .
     "}"
     (#make-range! "class.inner" @_start @_end))) @class.outer
+
+(struct_declaration
+  body: (declaration_list
+    .
+    "{"
+    .
+    "}"
+    )) @class.outer
+
+(record_declaration
+  body: (declaration_list
+    .
+    "{"
+    .
+    (_) @_start @_end
+    (_)? @_end
+    .
+    "}"
+    (#make-range! "class.inner" @_start @_end))) @class.outer
+
+(record_declaration
+  body: (declaration_list
+    .
+    "{"
+    .
+    "}"
+    )?) @class.outer
+
+(interface_declaration
+  body: (declaration_list
+    .
+    "{"
+    .
+    (_) @_start @_end
+    (_)? @_end
+    .
+    "}"
+    (#make-range! "class.inner" @_start @_end))) @class.outer
+
+(interface_declaration
+  body: (declaration_list
+    .
+    "{"
+    .
+    "}"
+    )) @class.outer
+
+(enum_declaration
+  body: (enum_member_declaration_list
+    .
+    "{"
+    .
+    (_) @_start @_end
+    (_)? @_end
+    .
+    "}"
+    (#make-range! "class.inner" @_start @_end))) @class.outer
+
+(enum_declaration
+  body: (enum_member_declaration_list
+    .
+    "{"
+    .
+    "}"
+    )) @class.outer
 
 (method_declaration
   body: (block

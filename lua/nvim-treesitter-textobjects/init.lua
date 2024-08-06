584fc6b213038eb9e1e0b32b89cfa6d595403d39
Christian Clason <c.clason@uni-graz.at>
2024-08-06 20:07:49 +0200

    refactor: consistent local api

--- lua/nvim-treesitter-textobjects/init.lua
+++ lua/nvim-treesitter-textobjects/init.lua
@@ -1,11 +1,9 @@
-local config = require "nvim-treesitter-textobjects.config"
-
 local M = {}
 
 ---@param options TSTextObjects.UserConfig?
 function M.setup(options)
   if options then
-    config.update(options)
+    require("nvim-treesitter-textobjects.config").update(options)
   end
 end
 

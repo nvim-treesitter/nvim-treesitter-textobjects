00e07287dda03a1c19412290bed297070a5ae47c
Christian Clason <c.clason@uni-graz.at>
2024-08-06 11:09:32 +0200

    fix(repeatable_move): remove deprecated functions
    Also stop advertising as generic API and point to nvim-next instead.

--- lua/nvim-treesitter-textobjects/repeatable_move.lua
+++ lua/nvim-treesitter-textobjects/repeatable_move.lua
@@ -1,17 +1,3 @@
--- If you are a plugin developer (or just want to make other movement plugins repeatable),
--- you can register your own independent move functions
--- even if they are not related to treesitter-textobjects.
--- Use one of the followings:
---   M.make_repeatable_move(move_fn)
---   M.make_repeatable_move_pair(forward_move_fn, backward_move_fn)
---   M.set_last_move(move_fn, opts, ...)
---
--- Then you can use four functions to repeat the last movement:
---   M.repeat_last_move
---   M.repeat_last_move_opposite
---   M.repeat_last_move_next
---   M.repeat_last_move_previous
-
 local M = {}
 
 ---@class TSTextObjects.RepeatableMove
@@ -126,15 +112,15 @@ M.repeat_last_move = function(opts_extend)
 
     if M.last_move.func == "f" or M.last_move.func == "t" then
       if opts.forward then
-        vim.cmd([[normal! ]] .. vim.v.count1 .. ";")
+        vim.cmd.normal { args = { vim.v.count1, ";" }, bang = true }
       else
-        vim.cmd([[normal! ]] .. vim.v.count1 .. ",")
+        vim.cmd.normal { args = { vim.v.count1, "," }, bang = true }
       end
     elseif M.last_move.func == "F" or M.last_move.func == "T" then
       if opts.forward then
-        vim.cmd([[normal! ]] .. vim.v.count1 .. ",")
+        vim.cmd.normal { args = { vim.v.count1, "," }, bang = true }
       else
-        vim.cmd([[normal! ]] .. vim.v.count1 .. ";")
+        vim.cmd.normal { args = { vim.v.count1, ";" }, bang = true }
       end
     else
       M.last_move.func(opts, unpack(M.last_move.additional_args))
@@ -195,143 +181,6 @@ M.builtin_T_expr = function()
     additional_args = {},
   }
   return "T"
-end
-
----@class TSTextObjects.BuiltinFindOpts
----@field forward boolean forward = true -> f, t
----@field inclusive boolean inclusive = false -> t, T
----@field char? string
----@field repeating? boolean if repeating with till (t or T, inclusive = false) then search from the next character
----@field winid? integer
-
--- implements naive f, F, t, T with repeat support
----@deprecated
----@param opts TSTextObjects.BuiltinFindOpts
----@return string? char
-local function builtin_find(opts)
-  local forward = opts.forward
-  local inclusive = opts.inclusive
-  local char = opts.char or vim.fn.nr2char(vim.fn.getchar())
-  local repeating = opts.repeating or false
-  local winid = opts.winid or vim.api.nvim_get_current_win()
-
-  if char == vim.fn.nr2char(27) then
-    -- escape
-    return nil
-  end
-
-  local line = vim.api.nvim_get_current_line()
-  local cursor = vim.api.nvim_win_get_cursor(winid)
-
-  -- count works like this with builtin vim motions.
-  -- weird, but we're matching the behaviour
-  local count ---@type integer
-  if not inclusive and repeating then
-    count = math.max(vim.v.count1 - 1, 1)
-  else
-    count = vim.v.count1
-  end
-
-  -- find the count-th occurrence of the char in the line
-  local found ---@type integer?
-  for _ = 1, count do
-    if forward then
-      if not inclusive and repeating then
-        cursor[2] = cursor[2] + 1
-      end
-      found = line:find(char, cursor[2] + 2, true)
-    else
-      -- reverse find from the cursor position
-      if not inclusive and repeating then
-        cursor[2] = cursor[2] - 1
-      end
-
-      found = line:reverse():find(char, #line - cursor[2] + 1, true)
-      if found then
-        found = #line - found + 1
-      end
-    end
-
-    if not found then
-      return char
-    end
-
-    if forward then
-      if not inclusive then
-        found = found - 1
-      end
-    else
-      if not inclusive then
-        found = found + 1
-      end
-    end
-
-    cursor[2] = found - 1
-    repeating = true -- after the first iteration, search from the next character if not inclusive.
-  end
-
-  -- Enter visual mode if we are in operator-pending mode
-  -- If we don't do this, it will miss the last character.
-  local mode = vim.api.nvim_get_mode()
-  if mode.mode == "no" then
-    vim.cmd "normal! v"
-  end
-
-  -- move to the found position
-  vim.api.nvim_win_set_cursor(winid, { cursor[1], cursor[2] })
-  return char
-end
-
--- We are not using M.make_repeatable_move and instead registering at M.last_move manually
--- because we don't want to behave the same way as the first movement.
--- For example, we want to repeat the search character given to f, F, t, T.
--- Also, we want to be able to to find the next occurence when using t, T with repeat, excluding the current position.
----@deprecated
-M.builtin_f = function()
-  vim.notify_once("nvim-treesitter-textobjects: map `builtin_f_expr` with `{expr=true}` instead.", vim.log.levels.WARN)
-  local opts = { forward = true, inclusive = true }
-  local char = builtin_find(opts)
-  if char ~= nil then
-    opts.char = char
-    opts.repeating = true
-    M.set_last_move(builtin_find, opts)
-  end
-end
-
----@deprecated
-M.builtin_F = function()
-  vim.notify_once("nvim-treesitter-textobjects: map `builtin_F_expr` with `{expr=true}` instead.", vim.log.levels.WARN)
-  local opts = { forward = false, inclusive = true }
-  local char = builtin_find(opts)
-  if char ~= nil then
-    opts.char = char
-    opts.repeating = true
-    M.set_last_move(builtin_find, opts)
-  end
-end
-
----@deprecated
-M.builtin_t = function()
-  vim.notify_once("nvim-treesitter-textobjects: map `builtin_t_expr` with `{expr=true}` instead.", vim.log.levels.WARN)
-  local opts = { forward = true, inclusive = false }
-  local char = builtin_find(opts)
-  if char ~= nil then
-    opts.char = char
-    opts.repeating = true
-    M.set_last_move(builtin_find, opts)
-  end
-end
-
----@deprecated
-M.builtin_T = function()
-  vim.notify_once("nvim-treesitter-textobjects: map `builtin_T_expr` with `{expr=true}` instead.", vim.log.levels.WARN)
-  local opts = { forward = false, inclusive = false }
-  local char = builtin_find(opts)
-  if char ~= nil then
-    opts.char = char
-    opts.repeating = true
-    M.set_last_move(builtin_find, opts)
-  end
 end
 
 return M

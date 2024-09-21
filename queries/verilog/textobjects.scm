ba77f10019f769db4d19104456da48393891e677
Henrykvdb <henrykvdb@hotmail.com>
2024-09-21 20:38:51 +0200

    feat(systemverilog): Fix textobjects - unify languages again

--- queries/verilog/textobjects.scm
+++ queries/verilog/textobjects.scm
@@ -1,17 +1,48 @@
-[
-  (loop_generate_construct)
-  (loop_statement)
-] @loop.outer
+(function_declaration
+  (function_body_declaration
+    (tf_port_list)
+    .
+    (_) @_start @_end
+    (_)? @_end
+    .
+    "endfunction"
+    (#make-range! "function.inner" @_start @_end))) @function.outer
+
+(task_declaration
+  (task_body_declaration
+    (tf_port_list)
+    .
+    (_) @_start @_end
+    (_)? @_end
+    .
+    "endtask"
+    (#make-range! "function.inner" @_start @_end))) @function.outer
 
 [
-  (conditional_statement)
-  (case_item)
-] @conditional.outer
+  (seq_block)
+  (generate_block)
+] @block.outer
+
+(seq_block
+  "begin"
+  (simple_identifier)?
+  .
+  (_) @_start @_end
+  (#not-kind-eq? @_start "simple_identifier")
+  (_)? @_end
+  .
+  "end"
+  (#make-range! "block.inner" @_start @_end))
+
+(generate_block
+  "begin"
+  (simple_identifier)?
+  .
+  (_) @_start @_end
+  (#not-kind-eq? @_start "simple_identifier")
+  (_)? @_end
+  .
+  "end"
+  (#make-range! "block.inner" @_start @_end))
 
 (comment) @comment.outer
-
-(function_declaration) @function.outer
-
-(always_construct) @block.outer
-
-(module_declaration) @class.outer

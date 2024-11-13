73367db215e12aa9a4f4c78cb52b270c6eec65a6
Corey McCourt <mccourtrc@gmail.com>
2024-11-12 19:20:47 -0800

    chore(gleam): capture comma for last parameter

--- queries/gleam/textobjects.scm
+++ queries/gleam/textobjects.scm
@@ -7,19 +7,40 @@
 
 ; parameter
 (function_parameters
+  .
+  (_) @parameter.inner
+  .
+  ","? @_end
+  (#make-range! "parameter.outer" @parameter.inner @_end))
+
+(function_parameters
+  "," @_start
+  .
+  (_) @parameter.inner
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+(arguments
+  .
   (_) @parameter.inner
   .
   ","? @_end
   (#make-range! "parameter.outer" @parameter.inner @_end))
 
 (arguments
+  "," @_start
+  .
+  (_) @parameter.inner
+  (#make-range! "parameter.outer" @_start @parameter.inner))
+
+(data_constructor_arguments
+  .
   (_) @parameter.inner
   .
   ","? @_end
   (#make-range! "parameter.outer" @parameter.inner @_end))
 
 (data_constructor_arguments
-  (_) @parameter.inner
+  "," @_start
   .
-  ","? @_end
-  (#make-range! "parameter.outer" @parameter.inner @_end))
+  (_) @parameter.inner
+  (#make-range! "parameter.outer" @_start @parameter.inner))

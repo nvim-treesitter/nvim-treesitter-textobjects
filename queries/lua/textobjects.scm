8b4e32e34d94fd2c4e2c898c6a0a1e60d2a0a181
Leon Strauss <me@lionc.de>
2025-02-28 16:45:15 +0100

    feat(lua): `@comment.inner` and `@statement.outer` (#728)

--- queries/lua/textobjects.scm
+++ queries/lua/textobjects.scm
@@ -22,7 +22,8 @@
 
 ; class
 ; comment
-(comment) @comment.outer
+(comment
+  (comment_content) @comment.inner) @comment.outer
 
 ; conditional
 (if_statement
@@ -121,3 +122,6 @@
 
 ; scopename
 ; statement
+(statement) @statement.outer
+
+(return_statement) @statement.outer

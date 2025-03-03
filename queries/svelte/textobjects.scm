146738f0f1561e3eca7881f56f466006892ffe92
Quang Phan <vnphanquang@gmail.com>
2025-03-04 00:49:27 +0700

    fix(svelte): remove non-standard queries

--- queries/svelte/textobjects.scm
+++ queries/svelte/textobjects.scm
@@ -3,14 +3,8 @@
 ; Svelte-specific text objects
 ; based on grammar defined at
 ; https://github.com/tree-sitter-grammars/tree-sitter-svelte
-; directives
-(attribute
-  (attribute_name) @_name
-  (expression)? @svelte.directive.inner
-  (#match? @_name "^(bind|use|transition|in|out|animate|style|class):.*$")) @svelte.directive.outer
-
 ; if block
-(if_statement) @svelte.block.outer @svelte.if.outer @conditional.outer
+(if_statement) @block.outer @conditional.outer
 
 (if_statement
   (if_start)
@@ -19,12 +13,11 @@
   (_)? @_end
   .
   (if_end)
-  (#make-range! "svelte.block.inner" @_start @_end)
-  (#make-range! "svelte.if.inner" @_start @_end)
+  (#make-range! "block.inner" @_start @_end)
   (#make-range! "conditional.inner" @_start @_end))
 
 ; each block
-(each_statement) @svelte.block.outer @svelte.each.outer @loop.outer
+(each_statement) @block.outer @loop.outer
 
 (each_statement
   (each_start)
@@ -33,12 +26,11 @@
   (_)? @_end
   .
   (each_end)
-  (#make-range! "svelte.block.inner" @_start @_end)
-  (#make-range! "svelte.each.inner" @_start @_end)
+  (#make-range! "block.inner" @_start @_end)
   (#make-range! "loop.inner" @_start @_end))
 
 ; key block
-(key_statement) @svelte.block.outer @svelte.key.outer
+(key_statement) @block.outer
 
 (key_statement
   (key_start)
@@ -47,11 +39,10 @@
   (_)? @_end
   .
   (key_end)
-  (#make-range! "svelte.block.inner" @_start @_end)
-  (#make-range! "svelte.key.inner" @_start @_end))
+  (#make-range! "block.inner" @_start @_end))
 
 ; await block
-(await_statement) @svelte.block.outer @svelte.await.outer
+(await_statement) @block.outer
 
 (await_statement
   (await_start)
@@ -60,11 +51,10 @@
   (_)? @_end
   .
   (await_end)
-  (#make-range! "svelte.block.inner" @_start @_end)
-  (#make-range! "svelte.await.inner" @_start @_end))
+  (#make-range! "block.inner" @_start @_end))
 
 ; snippet block
-(snippet_statement) @svelte.block.outer @svelte.snippet.outer
+(snippet_statement) @block.outer
 
 (snippet_statement
   (snippet_start)
@@ -73,5 +63,4 @@
   (_)? @_end
   .
   (snippet_end)
-  (#make-range! "svelte.block.inner" @_start @_end)
-  (#make-range! "svelte.snippet.inner" @_start @_end))
+  (#make-range! "block.inner" @_start @_end))

8e84a7485618cbea74217d3ad92d8750280939a2
Melker Ulander <melker.ulander@pm.me>
2025-03-12 18:42:38 +0100

    feat(jsx): add jsx/tsx element captures

--- queries/jsx/textobjects.scm
+++ queries/jsx/textobjects.scm
@@ -3,3 +3,22 @@
 (jsx_attribute
   (property_identifier)
   (_) @parameter.inner) @parameter.outer
+
+; Self closing tags
+(jsx_self_closing_element
+  name: (identifier)
+  .
+  (_) @_start
+  (_)* @_end
+  .
+  (#make-range! "jsx_element.inner" @_start @_end)) @jsx_element.outer
+
+; Paired tags
+(jsx_element
+  open_tag: (_)
+  .
+  (_) @_start
+  (_)* @_end
+  .
+  close_tag: (_)
+  (#make-range! "jsx_element.inner" @_start @_end)) @jsx_element.outer

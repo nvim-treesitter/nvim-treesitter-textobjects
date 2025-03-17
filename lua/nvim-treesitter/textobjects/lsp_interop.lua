9937e5e356e5b227ec56d83d0a9d0a0f6bc9cad4
Carmine Colucci <68693237+carminecolucci@users.noreply.github.com>
2025-03-17 02:46:01 +0100

    fix: typo `vim.api.util` to `vim.lsp.util` (#742)

--- lua/nvim-treesitter/textobjects/lsp_interop.lua
+++ lua/nvim-treesitter/textobjects/lsp_interop.lua
@@ -114,7 +114,7 @@ function M.peek_definition_code(query_string, query_group, lsp_request, context)
     local win_id = vim.api.nvim_get_current_win()
     local params = vim.fn.has "nvim-0.11" == 0 and vim.lsp.util.make_position_params()
       or function(client)
-        return vim.api.util.make_position_params(win_id, client.offset_encoding)
+        return vim.lsp.util.make_position_params(win_id, client.offset_encoding)
       end
     return vim.lsp.buf_request(
       0,

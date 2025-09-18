f66d511348dee0ba49cf8085fa70405750a3d975
purarue <7804791+purarue@users.noreply.github.com>
2025-09-18 13:37:48 -0700

    add block_attribute_declaration

--- queries/prisma/textobjects.scm
+++ queries/prisma/textobjects.scm
@@ -8,17 +8,20 @@
   (enum_block)
 ] @block.outer
 
-(enum_declaration) @class.outer
+[
+ (enum_declaration)
+ (model_declaration)
+ (type_declaration)
+] @class.outer
 
 (enum_block) @class.inner
-
-(enumeral) @parameter.inner @parameter.outer
-
-(model_declaration) @class.outer
 
 (model_declaration
   (statement_block) @class.inner)
 
-(column_declaration) @parameter.inner @parameter.outer
+(type_declaration
+  (statement_block) @class.inner)
 
-(type_declaration) @class.outer
+(enumeral) @parameter.inner @parameter.outer
+(column_declaration) @parameter.inner @parameter.outer
+(block_attribute_declaration) @parameter.outer

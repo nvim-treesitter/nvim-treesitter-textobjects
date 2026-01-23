49b97c22d67edf4310cd530b54118e3aa882f0bb
locnide <locnide@alpaga.dev>
2026-01-24 00:36:56 +0100

    feat(git_config): add @assignment, @block and @statement text objects

--- queries/git_config/textobjects.scm
+++ queries/git_config/textobjects.scm
@@ -1,1 +1,20 @@
+; assignments
+(variable
+  (_) @assignment.lhs @assignment.inner
+  value: (_) @assignment.rhs) @assignment.outer
+
+(variable
+  value: (_) @assignment.inner)
+
+; blocks
+(section
+  (section_header)
+  (_)+ @block.inner) @block.outer
+
+; comments
 (comment) @comment.outer
+
+; statements
+(section
+  (section_header)
+  (_) @statement.outer)

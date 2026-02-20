1fab75c08a9b64f9faa5db67862e368935f931ec
elgiano <elgiano@gmail.com>
2026-02-20 15:39:49 +0100

    fix(supercollider): update queries after breaking change

--- queries/supercollider/textobjects.scm
+++ queries/supercollider/textobjects.scm
@@ -1,58 +1,97 @@
-; conditional
-(if
-  true: (function_block)? @conditional.inner
-  false: (function_block)? @conditional.inner) @conditional.outer
+; assignment
+(variable_definition
+  name: (_) @assignment.lhs
+  value: (_) @assignment.inner @assignment.rhs) @assignment.outer
 
-(if
-  (_)? @conditional.inner) @conditional.outer
+(function_definition
+  name: (_) @assignment.lhs
+  value: (_) @assignment.inner @assignment.rhs) @assignment.outer
 
-; loops
-(for
-  (function_block)? @loop.inner) @loop.outer
-
-(while
-  (function_block)? @loop.inner) @loop.outer
-
+; block
 (code_block
-  (_) @block.inner) @block.outer
+  (_)* @block.inner) @block.outer
 
-(class) @class.outer
+; class
+(class_def
+  (class_def_body) @class.inner) @class.outer
 
-(parameter_call_list
-  (_)?) @class.inner
+; call
+(function_call
+  arguments: (_
+    .
+    "("
+    _+ @call.inner
+    ")")?) @call.outer
 
+; comment
 (line_comment) @comment.outer
 
 (block_comment) @comment.outer
 
-(function_block
-  (_) @function.inner) @function.outer
+; conditional:
+(function_call
+  name: (_) @_name
+  (#eq? @_name "if")
+  arguments: (parameter_call_list
+    (_) @conditional.inner)?) @conditional.outer
+
+; function
+((function_block) @function.inner @function.outer
+  (#offset! @function.inner 0 1 0 -1)) ; use offset to skip brackets
+
+; loop
+(function_call
+  name: (identifier) @_fname
+  (#eq? @_fname "while")
+  arguments: (parameter_call_list
+    (_) @loop.inner)?) @loop.outer
+
+(function_call
+  name: (identifier) @_fname
+  (#eq? @_fname "for")
+  arguments: (parameter_call_list
+    (_) @loop.inner .)?) @loop.outer
+
+(function_call
+  name: (identifier) @_fname
+  (#eq? @_fname "forBy")
+  arguments: (parameter_call_list
+    (_) @loop.inner .)?) @loop.outer
+
+; number
+(number) @number.inner
 
 ;parameters
 (parameter_call_list
-  "," @parameter.outer
-  .
-  (_) @parameter.inner @parameter.outer)
-
-(parameter_call_list
-  .
   (_) @parameter.inner @parameter.outer
   .
-  ","? @parameter.outer)
+  (",")? @parameter.outer)
 
-((symbol)?
-  (identifier)
+(parameter_call_list
   "," @parameter.outer
   .
-  ((method_name)
-    (function_call
-      (_))) @parameter.inner @parameter.outer)
+  (_) @parameter.inner @parameter.outer .)
 
-((symbol)?
-  (identifier)
+(parameter_list
+  (_) @parameter.inner @parameter.outer
   .
-  ((method_name)
-    (function_call
-      (_))) @parameter.inner @parameter.outer
+  (",")? @parameter.outer)
+
+(parameter_list
+  "," @parameter.outer
   .
-  ","? @parameter.outer)
+  (_) @parameter.inner @parameter.outer .)
+
+(collection
+  (_) @parameter.inner @parameter.outer
+  .
+  (",")? @parameter.outer)
+
+(collection
+  "," @parameter.outer
+  .
+  (_) @parameter.inner @parameter.outer .)
+
+; return
+(return_statement
+  (_) @return.inner) @return.outer
